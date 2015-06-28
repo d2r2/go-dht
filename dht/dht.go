eb08fd25d3504722668de7c574bc6332b78936d3
Denis Dyakov <denis.dyakov@gmail.com>
2015-06-29 00:04:47 +0600

    initial commit

---
+++ dht/dht.go
@@ -0,0 +1,169 @@
+package dht
+
+// #include "dht.go.h"
+// #cgo LDFLAGS: -lrt
+import "C"
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"time"
+	"unsafe"
+)
+
+type SensorType int
+
+const (
+	DHT11 SensorType = iota + 1
+	DHT22
+)
+
+type Pulse struct {
+	Value    bool
+	Duration time.Duration
+}
+
+func dialDHTxxAndGetResponse(pin int) (error, []Pulse) {
+	var arr *C.int32_t
+	var arrLen C.int32_t
+	var l []int32
+	// return array: [edge, duration, edge, duration...]
+	r := C.dial_DHTxx_and_read(4, &arr, &arrLen)
+	if r == -1 {
+		return fmt.Errorf("Error found in call to C.dial_DHTxx_and_read(...)"), nil
+	}
+	defer C.free(unsafe.Pointer(arr))
+	h := (*reflect.SliceHeader)(unsafe.Pointer(&l))
+	h.Data = uintptr(unsafe.Pointer(arr))
+	h.Len = int(arrLen)
+	h.Cap = int(arrLen)
+	pulses := make([]Pulse, len(l)/2)
+	// convert original array ([edge, duration...]) to edge list
+	for i := 0; i < len(l)/2; i++ {
+		pulses[i] = Pulse{Value: l[i*2] != 0,
+			Duration: time.Duration(l[i*2+1]) * time.Microsecond}
+	}
+	return nil, pulses
+}
+
+func decodeByte(pulses []Pulse, start int) (error, int) {
+	if len(pulses)-start < 16 {
+		return fmt.Errorf("Can't decode byte, since range beetwen "+
+			"index and array length is less than 16: %d, %d", start, len(pulses)), 0
+	}
+	var b int = 0
+	for i := 0; i < 8; i++ {
+		pulseL := pulses[start+i*2]
+		pulseH := pulses[start+i*2+1]
+		if pulseL.Value != false {
+			return fmt.Errorf("Low edge value expected at index %d", start+i*2), 0
+		}
+		if pulseH.Value != true {
+			return fmt.Errorf("High edge value expected at index %d", start+i*2+1), 0
+		}
+		const HIGH_DUR_MAX = (70 + (70 + 54)) / 2 * time.Microsecond
+		// Calc average value between 24us (bit 0) and 70us (bit 1).
+		// Everything that less this parameter is 0, bigger - 1.
+		const HIGH_DUR_AVG = (24 + (70-24)/2) * time.Microsecond
+		if pulseH.Duration > HIGH_DUR_MAX {
+			return fmt.Errorf("High edge value duration exceed "+
+				"expected maximum amount in %v: %v", HIGH_DUR_MAX, pulseH.Duration), 0
+		}
+		if pulseH.Duration > HIGH_DUR_AVG {
+			//fmt.Printf("bit %d is high\n", 7-i)
+			b = b | (1 << uint(7-i))
+		}
+	}
+	return nil, b
+}
+
+func decodeDHT11Pulses(pulses []Pulse) (err error, temp float32, hum float32) {
+	if len(pulses) == 84 {
+		pulses = pulses[2:82]
+	} else if len(pulses) == 83 {
+		pulses = pulses[1:81]
+	} else if len(pulses) != 82 {
+		return fmt.Errorf("Can't decode edge array, since incorrect length: %d",
+			len(pulses)), -1, -1
+	}
+	pulses = pulses[:80]
+	err, humInt := decodeByte(pulses, 0)
+	if err != nil {
+		return err, -1, -1
+	}
+	err, humDec := decodeByte(pulses, 16)
+	if err != nil {
+		return err, -1, -1
+	}
+	err, tempInt := decodeByte(pulses, 32)
+	if err != nil {
+		return err, -1, -1
+	}
+	err, tempDec := decodeByte(pulses, 48)
+	if err != nil {
+		return err, -1, -1
+	}
+	err, sum := decodeByte(pulses, 64)
+	if err != nil {
+		return err, -1, -1
+	}
+	if byte(sum) != byte(humInt+humDec+tempInt+tempDec) {
+		return fmt.Errorf("Control sum %d doesn't match %d+%d+%d+%d=%d\n",
+			sum, humInt, humDec, tempInt, tempDec,
+			byte(humInt+humDec+tempInt+tempDec)), -1, -1
+	}
+	temp = float32(tempInt)
+	hum = float32(humInt)
+	if hum > 100 {
+		return fmt.Errorf("Humidity value exceed 100%: %v", hum), -1, -1
+	}
+	return nil, temp, hum
+}
+
+func ReadDHTxx(sensorType SensorType, pin int) (err error,
+	temperature float32, humidity float32) {
+	err, pulses := dialDHTxxAndGetResponse(pin)
+	if err != nil {
+		return err, -1, -1
+	}
+	for i, pulse := range pulses {
+		fmt.Printf("pulse %d: %v, %v\n", i, pulse.Duration, pulse.Value)
+	}
+	err, temp, hum := decodeDHT11Pulses(pulses)
+	if err != nil {
+		return err, -1, -1
+	}
+	return nil, temp, hum
+}
+
+func ReadAndRetryDHTxx(sensorType SensorType, pin int, retry int) (err error,
+	temperature float32, humidity float32) {
+	var retryUsed int = 0
+	for {
+		err, temp, hum := ReadDHTxx(sensorType, pin)
+		if err != nil {
+			log.Println(err)
+			if retry > 0 {
+				retry--
+				retryUsed++
+				time.Sleep(1500 * time.Millisecond)
+				continue
+			}
+			return err, -1, -1
+		}
+		if retryUsed > 0 {
+			fmt.Printf("Has retried %d times\n", retryUsed)
+		}
+		return nil, temp, hum
+	}
+}
+
+func main() {
+	C.blink_n_times(25, 5)
+	err, temp, hum := ReadAndRetryDHTxx(DHT11, 4, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Temperature = %v*C, Humidity = %v%%\n", temp, hum)
+}
---
+++ dht/dht.go.h
@@ -0,0 +1,342 @@
+#ifndef GO_DHT_H
+#define GO_DHT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <time.h>
+
+// GPIO direction: receive either output data to specific GPIO pin.
+#define IN  0
+#define OUT 1
+ 
+// LOW correspong to low level of output signal, HIGH correspond to high level.
+#define LOW  0
+#define HIGH 1
+
+// Keep pin no, file descriptors for data reading/writing
+// and for specifying input/output mode.
+typedef struct {
+    int pin;
+    int fd_direction;
+    int fd_value;
+} Pin;
+ 
+// Freeze thread for usec microseconds
+static int sleep_usec(int32_t usec) {
+    struct timespec tim, tim2;
+    // convert microseconds to seconds
+    tim.tv_sec = usec / 1000000;
+    // rest part of microseconds convert to nanoseconds
+    tim.tv_nsec = (usec % 1000000) * 1000;
+    return nanosleep(&tim , &tim2);
+}
+
+// Start working with specific pin.
+static int gpio_export(int port, Pin *pin) {
+    #define BUFFER_MAX 3
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+                 
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open export for writing!\n");
+        return -1;
+    }
+    (*pin).pin = port;
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", (*pin).pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+
+    #define DIRECTION_MAX 35
+    char path1[DIRECTION_MAX];
+    snprintf(path1, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", (*pin).pin);
+    (*pin).fd_direction = open(path1, O_WRONLY);
+    if (-1 == (*pin).fd_direction) {
+        fprintf(stderr, "Failed to open gpio direction for writing!\n");
+        return -1;
+    }
+                             
+    #define VALUE_MAX 30
+    char path2[VALUE_MAX];
+    snprintf(path2, VALUE_MAX, "/sys/class/gpio/gpio%d/value", (*pin).pin);
+    (*pin).fd_value = open(path2, O_RDWR);
+    if (-1 == (*pin).fd_value) {
+        fprintf(stderr, "Failed to open gpio value for reading!\n");
+        return -1;
+    }
+                             
+    return 0;
+}
+
+// Stop working with specific pin.
+static int gpio_unexport(Pin *pin) {
+    close((*pin).fd_direction);
+    close((*pin).fd_value);
+
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+                 
+    fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (-1 == fd) {
+        fprintf(stderr, "Failed to open unexport for writing!\n");
+        return -1;
+    }
+                         
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", (*pin).pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return 0;
+}
+ 
+// Setup pin mode: input or output.
+static int gpio_direction(Pin *pin, int dir) {
+    static const char s_directions_str[]  = "in\0out";
+         
+    if (-1 == write((*pin).fd_direction, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
+        fprintf(stderr, "Failed to set direction!\n");
+        return -1;
+    }
+    return 0;
+}
+
+// Read data from the pin: in normal conditions return 0 or 1,
+// which correspond to low or high signal levels.
+static int gpio_read(Pin *pin) {
+    char value_str[3];
+                 
+    if (-1 == lseek((*pin).fd_value, 0, SEEK_SET)) {
+        fprintf(stderr, "Failed to seek file!\n");
+        return -1;
+    }
+    if (-1 == read((*pin).fd_value, value_str, 3)) {
+        fprintf(stderr, "Failed to read value!\n");
+        return -1;
+    }
+
+    // Small optimization to speed up GPIO processing
+    // due to ARM devices CPU slowness.
+    if (value_str[1] == '\0') {
+        return value_str[0] == '0' ? 0 : 1;
+    } else {
+        return atoi(value_str);
+    }
+}
+
+// Macro to convert timespec structure value to microseconds.
+#define convert_timespec_to_usec(t) ((t).tv_sec*1000*1000 + (t).tv_nsec/1000)
+ 
+// Read sequence of data from the pin trigering
+// on edge change until timeout occures.
+// Collect as well durations of pulses in microseconds.
+// Fill [arr] array with a sequence: level1, duration1, level2, duration2...
+// Put array length to variable [len].
+static int gpio_read_seq_until_timeout(Pin *pin,
+        int32_t timeout_msec, int32_t **arr, int32_t *len) {
+    int32_t last_v, next_v;
+#define MAX_PULSE_COUNT 16000
+    int values[MAX_PULSE_COUNT*2];
+    
+    last_v = gpio_read(pin);
+    if (-1 == last_v) {
+        fprintf(stderr, "Failed to read value!\n");
+        return -1;
+    }
+    int k = 0, i = 0;
+    values[k*2] = last_v;
+    struct timespec last_t, next_t;
+#define CLOCK_KIND CLOCK_MONOTONIC
+// #define CLOCK_KIND CLOCK_REALTIME
+    clock_gettime(CLOCK_KIND, &last_t);
+
+    for (;;)
+    {
+        // sleep_microsec(1);
+        next_v = gpio_read(pin);
+        if (-1 == next_v) {
+            fprintf(stderr, "Failed to read value!\n");
+            return -1;
+        }
+
+        if (last_v != next_v) {
+            clock_gettime(CLOCK_KIND, &next_t); 
+            i = 0;
+            k++;
+            if (k > MAX_PULSE_COUNT-1) {
+                fprintf(stderr, "Pulse count exceed limit in %d\n", MAX_PULSE_COUNT);
+                return -1;
+            }
+            values[k*2] = next_v;
+            // Save time duration in microseconds.
+            values[k*2-1] = convert_timespec_to_usec(next_t) -
+                convert_timespec_to_usec(last_t); 
+            last_v = next_v;
+            last_t = next_t;
+        }
+
+        if (i++ > 20) {
+            clock_gettime(CLOCK_KIND, &next_t); 
+            if ((convert_timespec_to_usec(next_t) -
+                convert_timespec_to_usec(last_t)) / 1000 > timeout_msec) {
+                values[k*2+1] = timeout_msec * 1000;
+                break;
+            }
+        }
+    }
+    *arr = malloc((k+1)*2 * sizeof(int32_t));
+    for (i=0; i<=k; i++)
+    {
+        (*arr)[i*2] = values[i*2];
+        (*arr)[i*2+1] = values[i*2+1];
+    }
+    *len = (k+1)*2;
+                                 
+/*    fprintf(stdout, "scan %d values\n", k+1);
+    for (i=0; i<=k; i++)
+    {
+        fprintf(stdout, "value %d (%d): %d\n", i, (*arr)[i*2+1], (*arr)[i*2]);
+    }*/
+    return 0;
+}
+ 
+// Set up specifi pin signal to 0 (low) or 1 (high).
+static int gpio_write(Pin *pin, int value) {
+    static const char s_values_str[] = "01";
+         
+    if (1 != write((*pin).fd_value, &s_values_str[LOW == value ? 0 : 1], 1)) {
+        fprintf(stderr, "Failed to write value!\n");
+        return -1;
+    }
+                                 
+    return 0;
+}
+
+// Used to set up maximum GPIO processing speed
+// during receiving bunch of data from sensors like DHTxx.
+static int set_max_priority(void) {
+    struct sched_param sched;
+    memset(&sched, 0, sizeof(sched));
+    // Use FIFO scheduler with highest priority
+    // for the lowest chance of the kernel context switching.
+    sched.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    if (-1 == sched_setscheduler(0, SCHED_FIFO, &sched)) {
+        fprintf(stderr, "Unable to set SCHED_FIFO priority to the thread\n");
+        return -1;
+    }
+    return 0;
+}
+
+// Get back normal thread priority.
+static int set_default_priority(void) {
+    struct sched_param sched;
+    memset(&sched, 0, sizeof(sched));
+    // Go back to regular schedule priority.
+    sched.sched_priority = 0;
+    if (-1 == sched_setscheduler(0, SCHED_OTHER, &sched)) {
+        fprintf(stderr, "Unable to set SCHED_OTHER priority to the thread\n");
+        return -1;
+    }
+    return 0;
+}
+
+typedef struct {
+    int time;
+    int edge;
+} edge_info;
+
+
+// Blink specific pin n times. Led could be
+// attached to this pin for debug purpose.
+static int blink_n_times(int pin, int n) {
+    Pin p;
+    if (-1 == gpio_export(pin, &p)) {
+        return -1;
+    }
+    if (-1 == gpio_direction(&p, OUT)) {
+        gpio_unexport(&p);
+        return -1;
+    }
+    int i;
+    // Blink led n times in a loop.
+    for (i = 0; i < n; i++)
+    {
+        // Turn led on
+        if (-1 == gpio_write(&p, HIGH)) {
+            gpio_unexport(&p);
+            return -1;
+        }
+        // Sleep 0.1 of second.
+        sleep_usec(100*1000);
+        // Turn led off
+        if (-1 == gpio_write(&p, LOW)) {
+            gpio_unexport(&p);
+            return -1;
+        }
+        // Sleep 0.1 of second.
+        sleep_usec(100*1000);
+    }
+    return gpio_unexport(&p);
+}
+
+// Activate DHTxx sensor and collect data sent by sensor for futher processing.
+static int dial_DHTxx_and_read(int pin, int32_t **arr, int32_t *arr_len) {
+    // Set maximum priority for GPIO processing.
+    if (-1 == set_max_priority()) {
+        return -1;
+    }
+    Pin p;
+    if (-1 == gpio_export(pin, &p)) {
+        set_default_priority();
+        return -1;
+    }
+    // Send dial pulse.
+    if (-1 == gpio_direction(&p, OUT)) {
+        gpio_unexport(&p);
+        set_default_priority();
+        return -1;
+    }
+    // Set pin to high.
+    if (-1 == gpio_write(&p, HIGH)) {
+        gpio_unexport(&p);
+        set_default_priority();
+        return -1;
+    }
+    // Sleep 500 millisecond.
+    sleep_usec(500*1000); 
+    // Set pin to low.
+    if (-1 == gpio_write(&p, LOW)) {
+        gpio_unexport(&p);
+        set_default_priority();
+        return -1;
+    }
+    // Sleep 18 milliseconds according to DHTxx specification.
+    sleep_usec(18*1000); 
+    // Switch pin to input mode
+    if (-1 == gpio_direction(&p, IN)) {
+        gpio_unexport(&p);
+        set_default_priority();
+        return -1;
+    }
+    // Read bunch of data from sensor
+    // for futher processing in high level language.
+    if (-1 == gpio_read_seq_until_timeout(&p, 10, arr, arr_len)) {
+        gpio_unexport(&p);
+        set_default_priority();
+        return -1;
+    }
+    // Release pin.
+    if (-1 == gpio_unexport(&p)) {
+        set_default_priority();
+        return -1;
+    }
+    // Return normal thread priority.
+    return set_default_priority();
+}
+
+
+
+#endif
